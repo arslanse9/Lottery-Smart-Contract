dcd2aa6bdc3cb99cde6aa4736d2e037c9924c0a6
Arslan <arslanm0909@gmail.com>
2023-08-01 17:05:33 +0500

    all files

---
+++ test/Lottery.test.js
@@ -0,0 +1,85 @@
+const ganache = require('ganache');
+const { Web3 } = require('web3');
+var assert = require('assert');
+const web3=new Web3(ganache.provider())
+
+const {interface,bytecode} =require("../compile")
+
+
+let lottery
+let accounts
+
+
+beforeEach(async ()=>{
+	accounts=await web3.eth.getAccounts()
+	lottery=await new web3.eth.Contract(JSON.parse(interface)).deploy({
+		data:bytecode,
+
+	}).send({from:accounts[0],gas:'1000000'})
+
+})
+
+describe('Lottery Contract',()=>{
+	it("deploy contracts",()=>{
+		assert.ok(lottery.options.address)
+	})
+
+	it("allow one account to send", async ()=>{
+		await lottery.methods.enter().send({
+			from:accounts[0],
+			value:web3.utils.toWei('0.02','ether')
+		})
+
+const players=await lottery.methods.getPlayers().call({
+	from:accounts[0]
+})
+
+assert.equal(accounts[0],players[0])
+assert.equal(1,players.length)
+
+	})
+it("it require minimum amount of ether to enter",async ()=>{
+	try{
+await lottery.methods.etner().send({
+	from:accounts[0],
+	value:0
+})
+assert(false)
+	}catch(err){
+assert(err)
+	}
+})
+it("only manager can pick winner",async ()=>{
+	try{
+
+await lottery.methods.enter.send({
+	from:accounts[0]
+})
+assert(false)
+
+	}catch(err)
+	{
+assert(err)
+	}
+})
+
+it("sends money to the winner and reset the players array", async()=>{
+	await lottery.methods.enter().send({
+		from:accounts[0],
+		value:web3.utils.toWei('2','ether')
+	})
+	const initialBalance=await web3.eth.getBalance(accounts[0])
+	await lottery.methods.pickWinner().send({
+		from:accounts[0]
+	})
+	const finalBalance=await web3.eth.getBalance(accounts[0])
+	const difference=finalBalance-initial.Balance
+	assert(difference>web3.utils.toWei('1.8','ether'))
+})
+
+
+
+
+
+
+})
